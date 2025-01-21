9304e43d33086a2420afcf3969a7f9a6fe2d54c9
Himanshu-00 <himanshuvinchurkar1387@gmail.com>
2025-01-21 20:23:51 +0530

    fix

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os 
 from gradio_interface import create_gradio_interface
+from model_loader import download_lora_model
 
 
 # Ensure model directories exist or create them if they don't
@@ -11,4 +12,5 @@ if not os.path.exists("debug_images"):
     os.makedirs("debug_images")
 
 if __name__ == "__main__":
+    download_lora_model()
     create_gradio_interface(share=True, debug=True)
