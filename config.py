836c50484218a878542e48e79f2a01d59b15bd8b
Himanshu-00 <himanshuvinchurkar1387@gmail.com>
2025-01-21 15:57:03 +0530

    steps

--- config.py
+++ config.py
@@ -9,5 +9,6 @@ CONFIG = {
     "lora_path": "models/lora/sd_xl_offset_example-lora_1.0.safetensors",  # Local path to save LoRA weights
     "debug_dir": "debug_images",  # Directory for debug images
     "guidance_scale" : 7.5,
+    "steps" : 30,
     "yolo_model_path": "yolov8n-face-lindevs.pt",  # YOLO model for face detection
 }
