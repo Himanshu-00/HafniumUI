09c177c4272c368c262d071859451fb8fc44682a
Himanshu-00 <himanshuvinchurkar1387@gmail.com>
2025-01-21 20:40:17 +0530

    fix

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import os 
-from gradio_interface import create_gradio_interface
+from gradio_interface import create_gradio_interface, pipeline_with_lora
+
+
 
 
 
 if __name__ == "__main__":
-    create_gradio_interface.launch(share=True, debug=True)
+    interface = create_gradio_interface(pipeline_with_lora)
+    interface.launch(share=True, debug=True)
