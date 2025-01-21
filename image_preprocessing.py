db4e0c919fd6777f1b35934396c7e3e8a504ab5f
Himanshu-00 <himanshuvinchurkar1387@gmail.com>
2025-01-21 20:27:41 +0530

    fix

--- image_preprocessing.py
+++ image_preprocessing.py
@@ -9,9 +9,13 @@ from config import YOLO_PATH
 # Load YOLO face detection model
 yolo_model = YOLO(YOLO_PATH)
 
-# Check debug directory
-DEBUG_DIR = "debug_images"
-os.makedirs(DEBUG_DIR, exist_ok=True)
+# Ensure model directories exist or create them if they don't
+if not os.path.exists("models"):
+    os.makedirs("models")
+if not os.path.exists("models/loras"):
+    os.makedirs("models/loras")
+if not os.path.exists("debug_images"):
+    os.makedirs("debug_images")
 
 def save_debug_image(image, name):
     """
--- main.py
+++ main.py
@@ -3,13 +3,6 @@ from gradio_interface import create_gradio_interface
 from model_loader import download_lora_model
 
 
-# Ensure model directories exist or create them if they don't
-if not os.path.exists("models"):
-    os.makedirs("models")
-if not os.path.exists("models/loras"):
-    os.makedirs("models/loras")
-if not os.path.exists("debug_images"):
-    os.makedirs("debug_images")
 
 if __name__ == "__main__":
     download_lora_model()
