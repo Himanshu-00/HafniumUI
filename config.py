ab18aa7eca694f46f2fadf4a797b093df981549f
Himanshu-00 <himanshuvinchurkar1387@gmail.com>
2025-01-21 15:53:59 +0530

    main.py

--- config.py
+++ config.py
@@ -8,5 +8,6 @@ CONFIG = {
     "lora_url": "https://huggingface.co/stabilityai/stable-diffusion-xl-base-1.0/resolve/main/sd_xl_offset_example-lora_1.0.safetensors",  # URL for LoRA weights
     "lora_path": "models/lora/sd_xl_offset_example-lora_1.0.safetensors",  # Local path to save LoRA weights
     "debug_dir": "debug_images",  # Directory for debug images
+    "guidance_scale" : 7.5,
     "yolo_model_path": "yolov8n-face-lindevs.pt",  # YOLO model for face detection
 }
