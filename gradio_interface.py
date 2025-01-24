1678a5064a0463be5b42fe34c86bbbc71ce0a5b6
Himanshu-00 <himanshuvinchurkar1387@gmail.com>
2025-01-24 16:49:01 +0530

    fix

--- gradio_interface.py
+++ gradio_interface.py
@@ -1,7 +1,7 @@
 # gradio_interface.py
 import gradio as gr
 from pipeline import generate_image_with_lora
-from config import PROMPT, STEPS, GS, NPROMPT
+from config import PROMPT, NPROMPT
 
 def create_gradio_interface(pipeline_with_lora):
     with gr.Blocks() as HeliumUI:
@@ -15,7 +15,7 @@ def create_gradio_interface(pipeline_with_lora):
                 # negative_prompt = gr.Textbox(label="Negative Prompt", placeholder="What you don't want in the image...")
                 with gr.Row():
                     guidance_scale = gr.Slider(minimum=1, maximum=20, value=7.5, step=0.5, label="Guidance Scale")
-                    steps = gr.Slider(minimum=1, maximum=100, value=30, step=1, label="Number of Steps")
+                    steps = gr.Slider(minimum=1, maximum=100, value=50, step=1, label="Number of Steps")
                 input_image = gr.Image(label="Input Image", tool="editor")
                 generate_btn = gr.Button("Generate Image with LoRA")
 
