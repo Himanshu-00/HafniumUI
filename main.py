4b99b337e527ba100d7fd01bad3326b7e91bfd0b
Himanshu-00 <himanshuvinchurkar1387@gmail.com>
2025-01-26 14:16:04 +0530

    fix

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
-#main.py
+# main.py
 from gradio_interface import create_gradio_interface
 from pipeline import pipeline_with_lora
 
-
-
-
 if __name__ == "__main__":
     HeliumUI = create_gradio_interface(pipeline_with_lora)
-    share_link = HeliumUI.launch(share=True, debug=True)
-
-    # Modify the share link to append dark theme
-    if share_link and isinstance(share_link, str):
-        dark_theme_link = f"{share_link}?__theme=dark"
-        print(f"Dark Theme URL: {dark_theme_link}")
+    public_url = HeliumUI.launch(share=True, debug=True)
+    
+    # Print dark theme URL
+    if public_url:
+        dark_theme_url = f"{public_url}?__theme=dark"
+        print(f"Dark Theme URL: {dark_theme_url}")
