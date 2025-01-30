384a58ace6ac46b5f9bbe85a735ab85cde7ca143
Himanshu-00 <himanshuvinchurkar1387@gmail.com>
2025-01-30 23:17:18 +0530

    fix

--- utlis.py
+++ utlis.py
@@ -3,9 +3,9 @@ from config import PROMPT
 
 
 #Prompt Handler
-def prompt_handler(gender, age, color_picker, color, guidance_scale, steps, input_image, num_outputs):
+def prompt_handler(gender, age, color_picker, guidance_scale, steps, input_image, num_outputs):
     # Replace placeholders in prompt
     prompt = PROMPT.replace("[Age]", str(age)) \
                          .replace("[Gender]", gender) \
                     
-    return generate_images(prompt, color_picker, guidance_scale, steps, input_image, num_outputs, color)
+    return generate_images(prompt, color_picker, guidance_scale, steps, input_image, num_outputs)
